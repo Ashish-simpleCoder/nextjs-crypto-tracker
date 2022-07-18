1bb8a925a806cb8f3b961063e9bc53b528700eb3
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2022-07-18 12:18:36 +0530

    [design] changed font weight of table body
    changed fontweight for table body row to 500 instead of default 600

--- src/components/shared/Table/Table.row.tsx
+++ src/components/shared/Table/Table.row.tsx
@@ -1,11 +1,18 @@
 import { memo, ReactNode } from "react"
+import styled from "styled-components"
 
 const TableRow = ({children}: {children: ReactNode})=>{
 
     return(
-        <tr>
+        <StyledRow>
             {children}
-        </tr>
+        </StyledRow>
     )
 }
 export default memo(TableRow)
+
+const StyledRow = styled.tr`
+    td{
+        font-weight: 500
+    }
+`
