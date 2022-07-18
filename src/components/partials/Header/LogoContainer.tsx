8e459329bb3e9d04e164ce1e2f4368a4a02433e6
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2022-07-18 11:02:06 +0530

    [added] button in logocontainer
    wrapped the heading and img in the button for accessibility

--- src/components/partials/Header/LogoContainer.tsx
+++ src/components/partials/Header/LogoContainer.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router'
 import { useCallback } from 'react'
 import styled from 'styled-components'
+import Button from '../../shared/Button'
 
 const LogoContainer = () => {
     const router = useRouter()
@@ -8,22 +9,22 @@ const LogoContainer = () => {
 
     return (
         <StyledLogoContainer onClick={goToHome}>
-            <span>
-                <img
-                    src='/imgs/cryptocurrencies.png'
-                    alt='crypto'
-                    title='<div>Icons made by <a href="https://www.flaticon.com/authors/dighital" title="Dighital">Dighital</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>'
-                />
-            </span>
-            <h1>Crypto Tracker</h1>
+            <Button>
+                <span>
+                    <img
+                        src='/imgs/cryptocurrencies.png'
+                        alt='crypto'
+                        title='<div>Icons made by <a href="https://www.flaticon.com/authors/dighital" title="Dighital">Dighital</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>'
+                    />
+                </span>
+                <h1>Crypto Tracker</h1>
+            </Button>
         </StyledLogoContainer>
     )
 }
 export default LogoContainer
 
 const StyledLogoContainer = styled.section`
-    display: flex;
-    align-items: center;
     cursor: pointer;
     span {
         margin-inline-end: 1rem;
@@ -32,5 +33,10 @@ const StyledLogoContainer = styled.section`
         color: var(--prm-clr);
         font-size: clamp(1.8rem, 2rem, 2vw);
         font-weight: 400;
+    }
+
+    button{
+        display: flex;
+        align-items: center;
     }
 `
