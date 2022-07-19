7928723378f68129b8e62e0c2cce441dee34c985
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2022-07-19 09:25:57 +0530

    [feat] loading the css file in _document
    loading the css file via the _document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import type { AppProps } from 'next/app'
 import Header from '../src/components/partials/Header/Header'
 import { CryptoContextProvider } from '../src/context/coinContext/coinContext'
 import { ThemeContextProvider } from '../src/context/theme/themeContext'
-import '../src/styles/index.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
